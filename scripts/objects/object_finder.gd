7abcd82be6b84c1c71989575609dd52d04bec251
Lazarev Daniil <daniil.kn2004@gmail.com>
2024-10-20 11:30:46 +0800

    Adjust doors collision areas

---
+++ scripts/objects/object_finder.gd
@@ -0,0 +1,15 @@
+class_name ObjectFinder extends Area2D
+
+signal on_object_found(character: Node2D, relation: Fraction.Relation) 
+
+signal on_object_lost(character: Node2D, relation: Fraction.Relation)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ scripts/objects/usable_data.gd
@@ -0,0 +1,3 @@
+class_name UsableData extends Resource
+
+@export var hint = "Use"
