f40aadd62713bcbfb756df2661f1442dd395df5b
Daniil Lazarev <daniil.kn2004@gmail.com>
2024-10-21 22:38:36 +0800

    Add ui selector for actions

--- scripts/ui/actions/actions_control.gd
+++ scripts/ui/actions/actions_control.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @export var character: Character
+@export var pointer: Control
 
 @onready var container: PanelContainer = get_node("ActionsContainer")
 @onready var sections_layout: VBoxContainer = get_node("ActionsContainer/UseActions")
@@ -8,6 +9,35 @@ extends Control
 var section_scene = preload("res://scenes/ui/actions/section.tscn")
 var sections: Array[ActionSection]
 var section_use_callables: Array[Callable]
+
+var pointer_idx: int = 0
+var pointer_action: Button = null
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("use") and pointer_action != null:
+		pointer_action.pressed.emit()	
+	if event.is_action_pressed("ui_next"):
+		set_pointer_index(pointer_idx + 1)
+
+func get_actions_list() -> Array[Button]:
+	var buttons: Array[Button] = []
+	var curr_idx = 0
+	for section in sections:
+		for action in section.actions.get_children():
+			buttons.append(action)
+	return buttons
+
+func set_pointer_index(idx: int):
+	var actions = get_actions_list()
+	if actions.is_empty():
+		pointer_action = null
+		pointer.visible = false
+		return
+	var index = idx % len(actions)
+	var action = actions[index]
+	pointer_idx = index
+	pointer_action = action
+	pointer.visible = true
 
 func _on_use_action(action: UsableAction, object: UsableArea):
 	object.use(character, action)
@@ -25,6 +55,11 @@ func _on_object_found(object: UsableArea):
 	
 	container.visible = true
 	
+	await get_tree().process_frame
+	
+	if pointer_action == null:
+		set_pointer_index(0)
+	
 func _on_object_lost(object: UsableArea):
 	var idx = -1
 	for section in sections:
@@ -39,7 +74,13 @@ func _on_object_lost(object: UsableArea):
 		
 	if sections.is_empty():
 		container.visible = false
-		
+	
+	set_pointer_index(pointer_idx)
+
+func _process(delta: float) -> void:
+	if pointer_action != null:
+		pointer.global_position = pointer_action.global_position \
+			+ Vector2(- pointer.size.x - 1, pointer_action.size.y / 2 - pointer.size.y / 2)
 
 func _ready() -> void:
 	container.visible = false
